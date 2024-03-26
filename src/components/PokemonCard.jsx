e901ca3e806d511c038ce88356aec67224c49d44
alexandre michiels <alexoualexandre1@gmail.com>
2024-03-27 00:04:10 +0100

    composant 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,22 @@
 
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+      
+    },
+  ];
+
+
 function PokemonCard(){
-   
+    const pokemon=pokemonList[0];
  return <figure>
-        <figcaption>bulbasaur</figcaption>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+        <figcaption> {pokemon.name} </figcaption>
+        {pokemon.imgSrc? <img src= {pokemon.imgSrc} /> : <p>???</p>}
         
     </figure>
 }
