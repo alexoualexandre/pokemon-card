462d92b482b54729ac348be052cf6998af018a69
alexandre michiels <alexoualexandre1@gmail.com>
2024-03-26 23:53:43 +0100

    composant 1

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,11 @@
+
+function PokemonCard(){
+   
+ return <figure>
+        <figcaption>bulbasaur</figcaption>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+        
+    </figure>
+}
+
+export default PokemonCard;
