97622b69f033d5c2b937ba4f1a7420786a302b96
alexoualexandre <143845250+alexoualexandre@users.noreply.github.com>
2024-03-26 22:52:49 +0100

    Delete src directory

--- src/components/PokemonCard.jsx
+++
@@ -1,8 +0,0 @@
-function PokemonCard(){
- return <figure>
-        <figcaption>Bulbasaur</figcaption>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-    </figure>
-}
-
-export default PokemonCard;
