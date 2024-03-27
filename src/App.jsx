f4d646d8a6a4a470959c93e24da75342216ba116
alexandre michiels <alexoualexandre1@gmail.com>
2024-03-27 11:55:37 +0100

    props1

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+      
+    },
+  ];
 
   return (
   <div>
-<PokemonCard/>
+<PokemonCard pokemon={pokemonList[0]} />
   </div>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,9 @@
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-      
-    },
-  ];
 
 
-function PokemonCard(){
-    const pokemon=pokemonList[0];
+
+function PokemonCard({pokemon}){
+  
  return <figure>
         <figcaption> {pokemon.name} </figcaption>
         {pokemon.imgSrc? <img src= {pokemon.imgSrc} /> : <p>???</p>}
