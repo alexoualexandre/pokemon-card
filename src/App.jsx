f58932a68c45897c2c5dcb08382c6bcd5b4ba12a
alexandre michiels <alexoualexandre1@gmail.com>
2024-03-28 21:29:53 +0100

    state1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 function App() {
-  
+  const [pokemonIndex, setPokemon] = useState(0);
   const pokemonList = [
     {
       name: "bulbasaur",
@@ -12,15 +12,37 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-      name: "mew",
-      
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
     },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    }
   ];
-
+  const next = () => {
+    setPokemon(pokemonIndex + 1);
+  }
+  const previous = () => {
+    setPokemon(pokemonIndex - 1);
+  }
   return (
-  <div>
-    <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc}/>
-  </div>
+
+    <div>
+      {pokemonIndex < pokemonList.length - 1 ? <button onClick={next}>suivant</button> : ""}
+      <PokemonCard name={pokemonList[pokemonIndex].name} imgSrc={pokemonList[pokemonIndex].imgSrc} />
+      {pokemonIndex > 0 ? <button onClick={previous}>précédent</button> : ""}
+    </div>
   )
 }
 
